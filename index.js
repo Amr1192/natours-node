6aefe6bf1cc1829f99d788c3b16f3e376ef1b7ac
Amr1192 <amr.ahmed11922911@gmail.com>
2026-06-21 15:13:57 +0300

    working

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const dotenv = require("dotenv")
 dotenv.config()
 const app = require("./express")
-<<<<<<< HEAD
 app.listen(process.env.PORT, ()=> console.log("server started 👌"))
-=======
-app.listen(process.env.PORT, ()=> console.log("server started 👌"))
->>>>>>> 356b3d541dc7a5857aece25aee4ce5a64e98d9ec
