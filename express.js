43d095e220f8d20f9c0f72d02a35a201271c217a
Amr1192 <amr.ahmed11922911@gmail.com>
2026-06-21 15:10:08 +0300

    start

---
+++ express.js
@@ -0,0 +1,44 @@
+const fs = require("fs")
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+const express = require("express")
+const app = express()
+
+app.get('/tours',(req,res)=> {
+    res.json(tours)
+})
+app.get('/tours/:id',(req,res)=> {
+    const id = Number(req.params.id)
+    if (id > tours.length - 1) {
+        return res.status(404).send("Tour not found")
+    }
+        res.json(tours[id])
+})
+
+app.post('/tours', (req,res) => {
+    const id = tours.length 
+    const newtour = {
+        id,
+        content: "hello from new post"
+    }
+    tours.push(newtour)
+    const result = JSON.stringify(tours)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,result,"utf-8",()=> {
+            res.json(tours)
+            })
+    })
+d
+
+module.exports = app
+const fs = require("fs")
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+const express = require("express")
+const app = express()
+app.get('/routes',(req,res)=> {
+    res.json(tours)
+})
+
+app.post('/tours', (req,res) => {
+    res.end(tours)
+})
+
+module.exports = app
